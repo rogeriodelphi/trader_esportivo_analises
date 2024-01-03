10f83864146624adeff17a26d6ca67db705db481
Rogério B. Martins <rogeriodelphi@gmail.com>
2024-01-03 04:01:52 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -26,10 +26,6 @@ def load_data(league, season):
   if selected_league == 'France':
     league = 'F1'
   
-  if selected_season == '2023/2024':
-    season = '2024'
-  if selected_season == '2022/2023':
-    season = '2023'  
   if selected_season == '2021/2022':
     season = '2122'
   if selected_season == '2020/2021':
