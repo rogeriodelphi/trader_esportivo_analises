d5a4601c6fb7ee19caebafe6cf8165aa0b43a6f0
Rogério B. Martins <rogeriodelphi@gmail.com>
2024-01-03 03:46:00 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.title('Web App Football Data')
+
 st.sidebar.header('Leagues')
+
 selected_league = st.sidebar.selectbox('League', ['England', 'Germany', 'Italy', 'Spain', 'France'])
 
 st.sidebar.header('Season')
@@ -12,12 +14,10 @@ selected_season = st.sidebar.selectbox('Season', ['2023/2024', '2022/2023', '202
 
 # webscraping Footeboll Data
 
-
-
 def load_data(league, season):
-  url = 'https://www.football-data.co.uk/mmz4281/*'season'*/+'league'+.csv'
+  url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
   data = pd.read_csv(url)
-  return data
+  return data 
 
 df =   load_data(selected_league, selected_season)
 
